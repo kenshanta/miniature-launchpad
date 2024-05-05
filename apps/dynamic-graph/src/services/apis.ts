3a070855f9d65b2c3f8b7d5bb6df4361c6395c92
Turbobot <turbobot@vercel.com>
2024-05-05 23:46:19 +0200

    feat(create-turbo): apply official-starter transform

--- apps/dynamic-graph/src/services/apis.ts
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export const API = axios.create({
-  baseURL: "https://data.ssb.no/api/v0/no/table/07241",
-  headers: { "Content-Type": "application/json" },
-  timeout: 10000,
-  withCredentials: false,
-});
--- apps/dynamic-graph/src/stores/housingStore.tsx
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import historyUrlReducer from "./historySlice";
-
-const housingStore = configureStore({
-  reducer: { history: historyUrlReducer },
-});
-
-export default housingStore;
